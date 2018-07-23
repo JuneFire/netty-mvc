96a5268f74ac90900ada257079d87a8b8f04afe4
程 <chengzhikang2015@163.com>
2018-07-23 22:15:15 +0800

    netty - mvc

---
+++ pro-netty-mvc/src/main/java/com/jary/netty/mvc/App.java
@@ -0,0 +1,13 @@
+package com.jary.netty.mvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
